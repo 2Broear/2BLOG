9a1e81326eb117962e5fd84a0bf39e39f5db61d2
2BROEAR <nukebattle1@gmail.com>
2022-06-20 15:04:11 +0800

    Update header.php

--- header.php
+++ header.php
@@ -49,13 +49,13 @@
                         $the_cat_slug = $the_cat->slug;  //use slug compare current category
                         $the_cat_par = get_category($the_cat->category_parent);
                         $catss = get_categories(meta_query_categories($the_cat_id, 'ASC', 'seo_order'));
-                        $slug_icon = $the_cat->slug!="/" ? $the_cat->slug : "more";
+                        $slug_icon = $the_cat_slug!="/" ? $the_cat_slug : "more";
                         if(!empty($catss)) $level="sec_level";else $level="top_level";
                         // 当前选中栏目 || 当前选中栏目下子栏目 || 当前栏目下文章&&文章单页
                         if($the_cat_id==$cat || cat_is_ancestor_of($the_cat_id, $cat) || in_category($the_cat_id)&&is_single()) $choosen="choosen";else $choosen = "";  //current category/page detect (bychild) DO NOT USE ID DETECT, because all cat are page(post) type;
                         $slash_link = get_category_link($the_cat_id)!=get_site_url() ? get_category_link($the_cat_id) : 'javascript:void(0)';  // detect if use $slash_link
                         $site_icon = $use_icon ? '<i class="icom icon-'.$slug_icon.'"></i>' : '';
-                        echo '<li class="cat_'.$the_cat_id.' '.$level.'"><a href="'.$slash_link.'" class="'.$choosen.'">' . $site_icon . $the_cat->name.'</a>';  //liwrapper
+                        if($the_cat_slug!='uncategorized') echo '<li class="cat_'.$the_cat_id.' '.$level.'"><a href="'.$slash_link.'" class="'.$choosen.'">' . $site_icon . $the_cat->name.'</a>';  //liwrapper
                         if(!empty($catss)){
                             if(get_option('site_metanav_switcher') && strpos(get_option('site_metanav_array'),$the_cat_slug)!==false){ //https://blog.csdn.net/ArthurBryant/article/details/6581833
                                 if(strpos(get_option('site_metanav_image'), $the_cat_slug)!==false) $metaCls = "metaboxes";else $metaCls="";  // must else for each-loop
