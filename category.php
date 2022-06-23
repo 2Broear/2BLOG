a0b4ef673aa8eb9ed319dba1eb9da4b32495cc52
2BROEAR <nukebattle1@gmail.com>
2022-06-23 22:44:43 +0800

    Update category.php
    fix level-4

--- category.php
+++ category.php
@@ -11,7 +11,15 @@
                 foreach($catss as $the_cats){
                     $the_cats_id = $the_cats->term_id;
                     $the_cats_temp = TEMPLATEPATH . '/'.get_term_meta($the_cats_id, 'seo_template', true);
-                    if(cat_is_ancestor_of($the_cats_id, $cat) && file_exists($the_cats_temp)) include_once($the_cats_temp);//else include_once(TEMPLATEPATH . '/category-default.php');
+                    if(cat_is_ancestor_of($the_cats_id, $cat) && file_exists($the_cats_temp)) include_once($the_cats_temp);
+                    $catsss = get_categories(meta_query_categories($the_cats_id, 'ASC', 'seo_order'));
+                    if(!empty($catsss)){
+                        foreach($catsss as $the_catss){
+                            $the_catss_id = $the_catss->term_id;
+                            $the_catss_temp = TEMPLATEPATH . '/'.get_term_meta($the_catss_id, 'seo_template', true);
+                            if(cat_is_ancestor_of($the_catss_id, $cat) && file_exists($the_catss_temp)) include_once($the_catss_temp);
+                        }
+                    }
                 }
             }
         }
