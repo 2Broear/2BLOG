198562f4612a92c567ea3e7dd76000b2517f2884
2BROEAR <nukebattle1@gmail.com>
2023-06-25 15:42:24 +0800

    Delete access_token.php

--- plugin/access_token.php
+++
@@ -1,6 +0,0 @@
-<?php
-$access_token = array (
-  'access_token' => '0TbdSyDeOP-SVJmIZtbdUK5mfvUAprSgBWUamytQf_TZsNCiHup0T_CiXJxBgWuX-lssqxtV6w0ymITgCzWnNX13E381Q-yBSH7Kf7B9avqGNWbkr3xCgwjHaqhlJ63ODO4MkZOQfdiHvK1usq6OAQ-VTlMfr8me8My8QWJnf7EquhYSH2e5X8ccJu88c50S_8VWBf1Xnz0X7JGjnWg12Q',
-  'expires' => 1682587508,
-);
-?>
